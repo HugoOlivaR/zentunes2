52462a18b5d5276907282e130e781aa0f615c338
hugoolv <hugoliva158@gmail.com>
2024-07-17 23:57:14 +0200

    cname

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  site: 'https://zentunes.es/',
   base: '/zentunes2/',
   plugins: [react()],
 })
